54dd687b9acf79ad0e7396611df2dba49a743027
ms0695580 <138789107+ms0695580@users.noreply.github.com>
2023-10-04 19:18:51 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo su
+yum update docker -y
 yum install docker -y
 systemctl start docker 
 systemctl enable docker
-docker run -d -p 8081:8081 --name nexus sonatype/nexus3
+docker run -d --restart always -p 8081:8081 --name nexus sonatype/nexus3
