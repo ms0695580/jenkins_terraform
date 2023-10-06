2b85cae4216cef3c410f0ac96e782942b932b132
ms0695580 <138789107+ms0695580@users.noreply.github.com>
2023-10-06 18:59:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-southeast-1"
-  access_key = "AKIAT4B5EL6AWNN7OJJJ"
-  secret_key = "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"
+  access_key = "AKIAT4B5EL6AUUKY7AGY"
+  secret_key = "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"
 }
